a006871bfa34ac1da1e649bcd4c764b30c05726a
dion.clawd <dion.christiann@gmail.com>
2026-04-20 19:51:51 +0700

    chore: sync setup.py version to 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="json-memory",
-    version="0.1.5",
+    version="1.4.0",
     description="Hierarchical associative memory for AI agents",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
