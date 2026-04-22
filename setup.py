aea6b97343f228ac63769b2590804b56b2b315b6
dion.clawd <dion.christiann@gmail.com>
2026-04-22 19:32:38 +0700

    chore: bump version to 1.9.1 after gap fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="json-memory",
-    version="1.9.0",
+    version = "1.9.1",
     description="Hierarchical associative memory for AI agents",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
